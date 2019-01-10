b0e5db43b489bf0473b861afd9f11e112bc06671
rubndubn <robinmantz@web.de>
2019-01-10 22:50:19 +0100

    Creating biderectional communication
    Datarate test is almost ready to be tested, a loop for continuous tests is missing, thats it

--- Robin/datarate_tests_receiver_AMB8826/datarate_tests_AMB8826_receiver.py
+++ Robin/datarate_tests_receiver_AMB8826/datarate_tests_AMB8826_receiver.py
@@ -1,6 +1,8 @@
 import time
+from threading import Timer
 import serial
 import AMB8826
+
 
 receiver = serial.Serial(  # initializes the receiver on COM4
     port='COM4',
@@ -15,11 +17,43 @@ if receiver.isOpen():
     receiver.isOpen()
 
 # ###################### USER CODE BEGIN ######################
+amount_of_messages = 20  # Setup for later
+received = []  # Setup for later
+while True:  # is running the whole time and is not intended to stop
+    keep_waiting = True  # Setup for later
+    received.clear()  # Reset the received after every measurement
+    start_time = 0  # Setup for later
+    line = AMB8826.get_answer_address_mode_1(receiver)  # always look for messages
+    if line.__sizeof__() != 0:  # is a message was received
+        if line[len(line)-3] == 255:  # The signal to start a new measurement
+            while keep_waiting:  # is active for one measurement
+                error_counter = 0  # reset error counter every time a new measurement is started
+                temp_line = AMB8826.get_answer_address_mode_1(receiver)  # Just a temp line for appending
+                received.append(temp_line[len(temp_line) - 3])  # Appending to the received list
+                print(received)
+                if received[len(received)-1] == 254:  # This is the signal, that the sender noticed a timeout
+                    print("Signal for timeout")
+                    keep_waiting = False  # End the measurement
+                    AMB8826.send_data(receiver, AMB8826.___cmd_data_req___from_hex("FF"))  # Wake up the sender
+                    print("Receiver got the timeout and woke up sender")
+                    keep_waiting = False
+                elif len(received) == amount_of_messages:  # the whole measurement data was received (excluding timeout)
+                    print("Trotzdem in IF rein ")
+                    for x in received:  # loop through the received message
+                        if received[x] != x:  # check for errors
+                            error_counter += error_counter
+                    if error_counter == 0:  # if there were no errors
+                        print("Successful measurement: ", received)
+                        AMB8826.send_data(receiver, AMB8826.___cmd_data_req___from_hex("FF"))  # Tell sender
+                        keep_waiting = False  # End measurement
+                    else:
+                        print("Error counter:  ", error_counter, "   What was received: ", received)
+                        keep_waiting = False  # End measurement
 
-while True:
-    print("Received (new): ", AMB8826.get_answer_address_mode_1(receiver))
 
 
 # ###################### USER CODE END ######################
 
 receiver.close()
+
+
--- Robin/datarate_tests_sender_AMB8826/datarate_tests_AMB8826_sender.py
+++ Robin/datarate_tests_sender_AMB8826/datarate_tests_AMB8826_sender.py
@@ -1,6 +1,11 @@
 import time
 import serial
 import AMB8826
+from threading import Timer
+
+
+def timeout():
+    AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_hex("FE"))
 
 
 sender = serial.Serial(  # initializes the sender on COM3
@@ -20,23 +25,31 @@ if sender.isOpen():
 
 # AMB8826.get_all_properties(sender)
 
-continue_test = True
-start_indication = "FF"
-message_filler =    "00112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566"  # 127 bytes
-while continue_test:
-    AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_string(start_indication))
-    start_time = int(round(time.time() * 1000))
-    x = 0
-    for x in range(26):
-        message = message_filler + '{0:02X}'.format(x)
-        AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_hex(message))
-        time.sleep(0.035)
-    answer = AMB8826.get_answer_address_mode_1(sender)
-    if answer[4] == 88:
-        end_time = int(round(time.time() * 1000))
-        duration = end_time - start_time
-        print(duration)
+amount_of_messages = 20  # Setup for later
+continue_test = True  # Setup for later
+start_indication = "FF"  # Setup for later
+message_filler = "0011223344556677889900112233445566778899001122334455667788990011223344556677889900" \
+                 "11223344556677889900112233445566778899001122334455667788990011223344556677889900" \
+                 "11223344556677889900112233445566778899001122334455667788990011223344556677889900" \
+                 "112233445566"  # 127 bytes  # Setup for later
+while continue_test:  # Is active while the test runs until the test ended
+    AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_string(start_indication))  # Sends the start signal
+    start_time = int(round(time.time() * 1000))  # Remembering the start point
+    x = 0  # Setup for later
+    for x in range(amount_of_messages):  # Sending the whole amount of messages
+        message = message_filler + '{0:02X}'.format(x)  # Filler plus the counter
+        AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_hex(message))  # sending
+        time.sleep(0.035)  # Wait until sending the next message
+    t = Timer(5, timeout)  # Initializing the Timer
+    t.start()  # Starting the timer
+    answer = AMB8826.get_answer_address_mode_1(sender)  # waiting for an answer
+    t.cancel()  # ending the timer
+    if answer[len(answer) - 3] == 255:  # if the receiver sent the signal for a finished measurement
+        end_time = int(round(time.time() * 1000))  # taking the end time
+        duration = end_time - start_time  # calculation duration
+        print("Duration: ", duration, "  Data rate: ", (128*amount_of_messages/(duration/1000)), " MB/s")
         continue_test = False
+        t.cancel()
 
 # ###################### USER CODE END ######################
 
