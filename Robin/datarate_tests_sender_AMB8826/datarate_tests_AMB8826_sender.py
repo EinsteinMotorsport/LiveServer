7c824dc42d4f764d271e15f085e48862e9759527
rubndubn <robinmantz@web.de>
2019-01-10 19:49:24 +0100

    Sending regular data and counting the time
    this is the first stage of implementing the datarate test. It is sending data to the receiver and logging the time that took if an answer was received, that was not implemented yet on the sender module

--- Robin/datarate_tests_sender_AMB8826/datarate_tests_AMB8826_sender.py
+++ Robin/datarate_tests_sender_AMB8826/datarate_tests_AMB8826_sender.py
@@ -18,13 +18,25 @@ if sender.isOpen():
 
 # ###################### USER CODE BEGIN ######################
 
-while True:
-    for x in range(1000):
-        AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_string('{0:02X}'.format(x)))
-        # AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_hex("01020203030304040404"))
-        time.sleep(0.05)
-    x = 0
+# AMB8826.get_all_properties(sender)
 
+continue_test = True
+start_indication = "FF"
+message_filler =    "00112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566778899001122334455667788990011223344556677889900112233445566"  # 127 bytes
+while continue_test:
+    AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_string(start_indication))
+    start_time = int(round(time.time() * 1000))
+    x = 0
+    for x in range(26):
+        message = message_filler + '{0:02X}'.format(x)
+        AMB8826.send_data(sender, AMB8826.___cmd_data_req___from_hex(message))
+        time.sleep(0.035)
+    answer = AMB8826.get_answer_address_mode_1(sender)
+    if answer[4] == 88:
+        end_time = int(round(time.time() * 1000))
+        duration = end_time - start_time
+        print(duration)
+        continue_test = False
 
 # ###################### USER CODE END ######################
 
